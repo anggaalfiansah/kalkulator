7cce006c41811faef481dac6f11929d7c17b1411
anggaalfiansah <anggaalfiansah26@gmail.com>
2020-12-29 16:25:03 +0700

    Update Final

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
 document.getElementById('kalkulator').addEventListener('submit', kalkulatorSederhana);
 
 function kalkulatorSederhana(e) {
-    const angka1           = document.getElementById('angka1');
-    const angka2           = document.getElementById('angka2');
-    const operator         = document.getElementById('operator');
-    const hasil            = document.getElementById('hasil');
+    const angka1           = parseInt(document.getElementById('angka1').value);
+    const angka2           = parseInt(document.getElementById('angka2').value);
+    const operator         = document.getElementById('operator').value;
 
     const penjumlahan = angka1 + angka2;
     const pengurangan = angka1 - angka2;
+    const perkalian   = angka1 * angka2;
+    const pembagian   = angka1 / angka2;
+
+    hasil = document.getElementById('hasil');
+    if(operator == '+') {
+        hasil.value = penjumlahan;
+    }
+    else if(operator == '-') {
+        hasil.value = pengurangan;
+    }
+    else if(operator == '*') {
+        hasil.value = perkalian;
+    }
+    else if(operator == '/') {
+        hasil.value = pembagian;
+    }
     
     e.preventDefault();
 }
